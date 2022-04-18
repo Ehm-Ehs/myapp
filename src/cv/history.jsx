3e301d87ce30fe54cdc850d92e27c3dbfcf3bc6e
Ehm Ehs <salamimariam239@gmail.com>
2022-04-18 20:45:50 +0100

    initial

---
+++ src/cv/history.jsx
@@ -0,0 +1,15 @@
+function History(){
+  return(
+    <>
+    <title>Employment History</title>
+      <table>
+        <tr>
+          <td> Secretary</td>
+          <td> Connection Tutorials</td>
+        </tr>
+      </table>
+      </>
+    );
+  }
+
+export default History;
---
+++ src/cv/intro.jsx
@@ -0,0 +1,54 @@
+function Intro(){
+  return(
+    <>
+    <div>SALAMI M.K.</div>
+    <div>
+      <table cellspacing="30">
+        <tr>
+          <td> <img src="mariam pics.png" alt="Mariam Profile pics"/> </td>
+          <td>  <h1>SALAMI MARIAM KOFOWOROLA</h1>
+            <h3> <strong>33, Area A, Nyanya Abuja. 08073603859. 08092096510. salamimariamkofoworola@gmail.com</strong> </h3></td>
+        </tr>
+      </table>
+    </div>
+    <div>
+       <p>A focused, enthusiastic and result-driven graduate of sociology</p>
+        <p> I have posses graphics designing,researcing, typing, softskills,leadership and presentation skills </p>
+        <p> I love reading, listening to music and watching movies </p>
+    </div>
+    <hr/>
+    <div>
+      <p> <h2> Education </h2> </p>
+    <ul>
+     <li><p> <strong> <a href="https://www.unilorin.edu.ng/">University of Ilorin</a> </strong> </p>
+     <p> <em> September 2014 - October 2018 </em> </p>
+     <p> <em> Bsc. Sociology</em> </p> </li>
+     <li> <p> <strong> <a href="https://lagosschoolsonline.com/schools/profile/105670"> Lagos State Government Secondary School Ijanikin</a></strong> </p>
+     <p> <em> May 2008 - June 2014</em> </p>
+      <p> <em>SSCE</em> </p> </li>
+    </ul>
+    <hr/>
+    <p> <h2> Softwares Proficency</h2> </p>
+    <table cellspacing="60">
+     <thead>
+       <td>Microsoft Office</td>
+       <td>⭐⭐⭐⭐⭐</td>
+        <tr>
+          <td> Corel Draw </td>
+          <td>⭐⭐⭐⭐</td>
+        </tr>
+        <tr>
+          <td>Adobe Ilustrator</td>
+          <td>⭐⭐⭐⭐</td>
+        </tr>
+     </thead>
+     <hr/>
+     <p> <h2></h2> </p>
+    </table>
+    <hr/>
+    <p> <strong> <a href="employment history.html">Employment History</a> </strong> </p>
+  </div>
+  </>
+  );
+}
+export default Intro;
